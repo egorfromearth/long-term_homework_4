6fd01b83fce15b3b18dd3ba4f0afc15b3ad445c1
egorfromearth <52272806+egorfromearth@users.noreply.github.com>
2019-10-24 21:03:45 +0300

    Классы без реализации
    Классы без реализации 1 раз

---
+++ Agent/Agent/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Agent
+{
+       
+    class Agent
+    {
+        private int _idAgent;
+   
+        private Dictionary<int, string> tasks; //Id/hash
+        public int IdAgent { get => _idAgent; set => _idAgent = value; }
+
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+
+    struct systemInformation
+    {
+          int _core;
+          double _passwordsPerSecond;
+    }
+
+    class PasswordGuessing
+    {
+        private string _alphabet;
+        private string _hash;
+        private string _password;
+
+        public PasswordGuessing(string alphabet, string hash)
+        {
+            _alphabet = alphabet;
+            _hash = hash;
+            _password = "";
+        }
+        public bool BruteForce(string beginRange, string endRange, string password)
+        {
+            return true;
+        }
+
+        public string getPassword()
+        {
+            return _password;
+        }
+    }
+
+    class Message
+    {
+        private int _idRecepient;
+        private int _idSender;
+        private string _message;
+        public Message(int idRecepient, int idSender, string message)
+        {
+            _idRecepient = idRecepient;
+            _idSender = idSender;
+            _message = message;
+        }
+
+        public virtual string[] getData()
+        {
+            return null;
+        }
+    }
+
+    class Communication
+    {
+       bool SendMessage(Message message, string path) //путь к очереди
+        {
+            return true;
+        }
+
+        Message CheckMessage(string path)
+        {
+            return null;
+        }
+        
+    }
+
+}
+
+
+
---
+++ ControllAplication/ControllAplication/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ControllAplication
+{
+
+    class ControlApplication
+    {
+        private List<Task> _tasksList;
+        private List<Agent> _tasksAgent;
+
+        public void distributionTasks() { }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+    struct Task
+    {
+        bool _complete;
+        string _hash;
+        int _idTask;
+        bool _processing;
+        string readyPassword;
+    }
+
+
+    struct Agent
+    {
+        int _idAgent;
+        int _idTask;
+        systemInformation info;
+    }
+    struct systemInformation
+    {
+        int _core;
+        double _passwordsPerSecond;
+    }
+
+
+
+
+
+
+
+    class Message
+    {
+        private int _idRecepient;
+        private int _idSender;
+        private string _message;
+        public Message(int idRecepient, int idSender, string message)
+        {
+            _idRecepient = idRecepient;
+            _idSender = idSender;
+            _message = message;
+        }
+
+        public virtual string[] getData()
+        {
+            return null;
+        }
+    }
+
+    class Communication
+    {
+        bool SendMessage(Message message, string path) //путь к очереди
+        {
+            return true;
+        }
+
+        Message CheckMessage(string path)
+        {
+            return null;
+        }
+
+    }
+
+}
